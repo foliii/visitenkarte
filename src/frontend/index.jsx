0ab5623b5444a0c4eec83913f0d89212c55f3844
kelly <kellysophiakoch@icloud.com>
2025-12-08 23:02:42 +0100

    chore: add build via react scripts

---
+++ src/frontend/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QRCodeComponent from '../qrCode';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Visitenkarte</h1>
+      <QRCodeComponent/>
+    </div>
+  );
+};
+
+export default App;
