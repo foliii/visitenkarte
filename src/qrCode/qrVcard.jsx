5e8e1270a13165e6252e1c3b38a6e6ed8d3cd071
on23Bfe <fri.egner.23@lehre.mosbach.dhbw.de>
2025-12-16 17:19:39 +0100

    e-mail, tel hinzugefügt, vorschau geändert

--- src/qrCode/qrVcard.jsx
+++ src/qrCode/qrVcard.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useRef } from 'react';
 import QRCode from 'qrcode';
-import "./style.css";
 
 const QRCodeComponent = ({ data }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    if (canvasRef.current && data) {
-      const vCardData = `BEGIN:VCARD\nVERSION:3.0\nN:${data.lastName};${data.firstName}\nTITLE:${data.position}\nEND:VCARD`;
-      QRCode.toCanvas(canvasRef.current, vCardData, { width: 256 }, (error) => {
+    if (!canvasRef.current || !data) return;
+
+    const vCardData = `
+BEGIN:VCARD
+VERSION:3.0
+N:${data.lastName};${data.firstName}
+FN:${data.firstName} ${data.lastName}
+TITLE:${data.position}
+${data.phone ? `TEL;TYPE=CELL:${data.phone}` : ''}
+${data.email ? `EMAIL;TYPE=INTERNET:${data.email}` : ''}
+END:VCARD
+`.trim();
+
+    QRCode.toCanvas(
+      canvasRef.current,
+      vCardData,
+      { width: 140 },
+      (error) => {
         if (error) console.error(error);
-      });
-    }
+      }
+    );
   }, [data]);
 
-  return (
-    <div>
-      <canvas ref={canvasRef}></canvas>
-    </div>
-  );
+  return <canvas ref={canvasRef}></canvas>;
 };
 
 export default QRCodeComponent;
