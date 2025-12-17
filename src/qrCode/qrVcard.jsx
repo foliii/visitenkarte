19ff2f2e23b21adb08b141a789fef4bbb821cabf
on23Bfe <fri.egner.23@lehre.mosbach.dhbw.de>
2025-12-17 14:27:46 +0100

    Event- und Geo-QR-Code ergänzt

--- src/qrCode/qrVcard.jsx
+++ src/qrCode/qrVcard.jsx
@@ -7,7 +7,36 @@ const QRCodeComponent = ({ data }) => {
   useEffect(() => {
     if (!canvasRef.current || !data) return;
 
-    const vCardData = `
+    let qrContent = '';
+
+    switch (data.qrType) {
+      case 'mail':
+        qrContent = `mailto:${data.email}`;
+        break;
+
+      case 'url':
+        qrContent = 'https://www.mosbach.dhbw.de/'; 
+        break;
+
+      case 'location':
+        qrContent = 'geo:49.3536,9.1459'; 
+        break;
+
+      case 'event':
+        qrContent = `
+BEGIN:VEVENT
+SUMMARY:Business Meeting
+DTSTART:20250115T100000
+DTEND:20250115T110000
+LOCATION:DHBW Mosbach
+DESCRIPTION:Beispieltermin für Visitenkarten-QR-Code
+END:VEVENT
+`.trim();
+        break;
+
+      case 'vcard':
+      default:
+        qrContent = `
 BEGIN:VCARD
 VERSION:3.0
 N:${data.lastName};${data.firstName}
@@ -17,10 +46,11 @@ ${data.phone ? `TEL;TYPE=CELL:${data.phone}` : ''}
 ${data.email ? `EMAIL;TYPE=INTERNET:${data.email}` : ''}
 END:VCARD
 `.trim();
+    }
 
     QRCode.toCanvas(
       canvasRef.current,
-      vCardData,
+      qrContent,
       { width: 140 },
       (error) => {
         if (error) console.error(error);
